9a3fecb33ded6a324db0a96cd0400e9d0eae90e7
Akos-1 <veronicagyasi@gmail.com>
2024-04-07 12:56:16 -0700

    updated

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel(app)
+
+SUPPORTED_LANGUAGES = ['en', 'fr']
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(SUPPORTED_LANGUAGES)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html',
+                           title=_('home_title'),
+                           header=_('home_header'))
+
+
+if __name__ == '__main__':
+    app.run()
